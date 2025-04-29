989f3cb45a951cbf7da7877402dbd85ac5c2283b
Devin Cummings <dcummings1015@gmail.com>
2025-04-29 16:48:00 -0400

    Added very rudimentary movement for both characters

---
+++ final-project/SwimmingPlayer.gd
@@ -0,0 +1,34 @@
+extends CharacterBody2D
+
+@export var acceleration = 500.0 # acceleration in pixels/second/second
+@export var max_velocity = 1000.0 # max velocity in pixels/second
+@export var push_force = 100.0
+
+func _physics_process(delta):
+	var input_direction = Vector2(
+		Input.get_axis("FishLeft", "FishRight"),
+		Input.get_axis("FishUp", "FishDown")
+	)
+
+	if input_direction != Vector2.ZERO:
+		input_direction = input_direction.normalized()
+		velocity += input_direction * acceleration * delta
+	else:
+		# Optionally add friction or damping when not pressing input
+		velocity = velocity.move_toward(Vector2.ZERO, acceleration * delta)
+
+	# Clamp to max velocity
+	if velocity.length() > max_velocity:
+		velocity = velocity.normalized() * max_velocity
+
+	move_and_slide()
+	push_objects()
+
+func push_objects():
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		var collider = collision.get_collider()
+		if collider is RigidBody2D:
+			collider.apply_central_impulse(collision.get_normal() * -1 * push_force)
+		elif collider is StaticBody2D:
+			velocity = Vector2.ZERO
