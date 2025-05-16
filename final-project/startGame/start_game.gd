86c90e194987b321a734f955f329cafcb26ae046
Yanelis Otero <121064351+FutureNormal830@users.noreply.github.com>
2025-05-16 14:48:42 -0400

    Added decorations and finished start screen
    Add decorations:
     - shell
     - seaweed
     - cloud
     - rocks
    
    Finished start screen, now when pressing the start button will load the next scene

---
+++ final-project/startGame/start_game.gd
@@ -0,0 +1,23 @@
+extends CanvasLayer
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	#ProjectSettings.set_setting("display/window/size/window_width_override", 1100)
+	#ProjectSettings.set_setting("display/window/size/window_height_override", 300)
+	
+	get_window().size = Vector2i(1100, 650)
+	
+	$seaweed.play("default")
+	$seaweed2.play("default")
+	$AnimatedSprite2D.play("start")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://IntroStory.tscn")
