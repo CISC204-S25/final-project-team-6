6f8ec4c88f0e1edf61adf6e62498eeb9ae951827
Devin Cummings <dcummings1015@gmail.com>
2025-05-20 18:02:23 -0400

    Made players playable in all levels

--- final-project/bedroom/bedroom.gd
+++ final-project/bedroom/bedroom.gd
@@ -2,10 +2,26 @@ extends Node2D
 signal swimming
 signal notSwimming
 
+@onready var players := {
+	"1": {
+		viewport = $"HBoxContainer/SubViewportContainer/SubViewport1",
+		camera = $"HBoxContainer/SubViewportContainer/SubViewport1/Camera2D1",
+		player = $HBoxContainer/SubViewportContainer/SubViewport1/pelican,
+	},
+	"2" : {
+		viewport = $"HBoxContainer/SubViewportContainer2/SubViewport2",
+		camera = $"HBoxContainer/SubViewportContainer2/SubViewport2/Camera2D2",
+		player = $HBoxContainer/SubViewportContainer2/SubViewport2/pufferfish,
+	}
+}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	players["2"]["viewport"].world_2d = players["1"]["viewport"].world_2d
+	for node in players.values():
+			var remote_transform := RemoteTransform2D.new()
+			remote_transform.remote_path = node["camera"].get_path()
+			node["player"].add_child(remote_transform)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- final-project/party_room/party_room.gd
+++ final-project/party_room/party_room.gd
@@ -5,10 +5,26 @@ signal puffed_changed
 
 var is_puffed = false
 
+@onready var players := {
+	"1": {
+		viewport = $"HBoxContainer/SubViewportContainer/SubViewport1",
+		camera = $"HBoxContainer/SubViewportContainer/SubViewport1/Camera2D1",
+		player = $HBoxContainer/SubViewportContainer/SubViewport1/pelican,
+	},
+	"2" : {
+		viewport = $"HBoxContainer/SubViewportContainer2/SubViewport2",
+		camera = $"HBoxContainer/SubViewportContainer2/SubViewport2/Camera2D2",
+		player = $HBoxContainer/SubViewportContainer2/SubViewport2/pufferfish,
+	}
+}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	players["2"]["viewport"].world_2d = players["1"]["viewport"].world_2d
+	for node in players.values():
+			var remote_transform := RemoteTransform2D.new()
+			remote_transform.remote_path = node["camera"].get_path()
+			node["player"].add_child(remote_transform)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -26,8 +42,8 @@ func _on_swimming_body_exited(body: Node2D) -> void:
 
 func _on_be_puffed_area_body_entered(body: Node2D) -> void:
 	if(is_puffed == true):
-		$background2.show()
-		$lampStaticBody.hide()
+		$HBoxContainer/SubViewportContainer/SubViewport1/level/background2.show()
+		$HBoxContainer/SubViewportContainer/SubViewport1/level/lampStaticBody.hide()
 
 func _on_exit_area_body_entered(body: Node2D) -> void:
 	#leads back to the vent room
